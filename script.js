26fd77d52877fffcec1f61dbc4109b3935539181
Sujon Pramanik <91121755+sujon-cloud@users.noreply.github.com>
2025-07-15 17:10:52 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// trogle menu
+const nav = document.querySelector(".nav-links");
+const navButton = document.querySelector(".nav-button");
+
+navButton.addEventListener("click", function () {
+  nav.classList.toggle("show-list")});
